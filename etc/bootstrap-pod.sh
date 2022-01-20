23b94077b2d8f671bc62c745b5a740dc6eaac83b
Lewis Sobotkiewicz <lsobotkiewicz@kixeye.com>
2022-01-19 23:19:14 -0800

    Update bootstrap-pod.sh

--- etc/bootstrap-pod.sh
+++ etc/bootstrap-pod.sh
@@ -1,34 +1,30 @@
 #!/bin/sh
-set -e
+set -ex
 
-# Find which member of the PetSet this pod is running
+# Find which member of the Stateful Set this pod is running
 # e.g. "redis-cluster-0" -> "0"
-PET_ORDINAL=$(cat /etc/podinfo/pod_name | cut -d- -f3)
+PET_ORDINAL=$(cat /etc/podinfo/pod_name | rev | cut -d- -f1)
+MY_SHARD=$(($PET_ORDINAL % $NUM_SHARDS))
 
 redis-server /conf/redis.conf &
 
 # TODO: Wait until redis-server process is ready
 sleep 1
 
-if [ $PET_ORDINAL = "0" ]; then
-  # The first member of the cluster should control all slots initially
-  echo "Bootstrapping this cluster node with all cluster slots..."
-  redis-cli cluster addslots $(seq 0 16383)
-else
-  # TODO: Get list of peers using the peer finder using an init container
-  PEER_IP=$(perl -MSocket -e 'print inet_ntoa(scalar(gethostbyname("redis-cluster-0.redis-cluster.default.svc.cluster.local")))')
- 
-  # TODO: Make sure the node we're initializing is not already a master (it may be a recovering node)
-  redis-cli cluster meet $PEER_IP 6379
-  sleep 1
-  
-  #echo redis-cli --csv cluster slots
-  #redis-cli --csv cluster slots
+if [ $PET_ORDINAL -lt $NUM_SHARDS ]; then
+  # Set up primary nodes. Divide slots into equal(ish) contiguous blocks
+  NUM_SLOTS=$(( 16384 / $NUM_SHARDS ))
+  REMAINDER=$(( 16384 % $NUM_SHARDS ))
+  START_SLOT=$(( $NUM_SLOTS * $MY_SHARD + ($MY_SHARD < $REMAINDER ? $MY_SHARD : $REMAINDER) ))
+  END_SLOT=$(( $NUM_SLOTS * ($MY_SHARD+1) + ($MY_SHARD+1 < $REMAINDER ? $MY_SHARD+1 : $REMAINDER) - 1 ))
 
-  # Become the slave of a random master node
-  MASTER_ID=$(redis-cli --csv cluster slots | cut -d, -f 5 | sed -e 's/^"//'  -e 's/"$//')
-  redis-cli cluster replicate $MASTER_ID
+  PEER_IP=$(perl -MSocket -e "print inet_ntoa(scalar(gethostbyname(\"redis-cluster-0.redis-cluster.$POD_NAMESPACE.svc.cluster.local\")))")
+  redis-cli cluster meet $PEER_IP 6379
+  redis-cli cluster addslots $(seq $START_SLOT $END_SLOT)
+else
+  # Set up a replica
+  PEER_IP=$(perl -MSocket -e "print inet_ntoa(scalar(gethostbyname(\"redis-cluster-$MY_SHARD.redis-cluster.$POD_NAMESPACE.svc.cluster.local\")))")
+  redis-cli --cluster add-node localhost:6379 $PEER_IP:6379 --cluster-slave
 fi
 
 wait
-
